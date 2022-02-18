42b46e6b9a3aef9b30e2d0156edbded5d32785dd
luyb <luyb@xunzhaotech.com>
2022-02-18 17:27:04 +0800

    fix -修改基础信息内容设置

--- src/layouts/UserLayout/index.vue
+++ src/layouts/UserLayout/index.vue
@@ -18,10 +18,12 @@
 </template>
 
 <script lang="ts">
+import { Options, Vue } from "vue-class-component";
 import { computed, defineComponent, reactive, ref, watchEffect } from "vue";
 import { useRouter } from "vue-router";
+import type { RouteContextProps } from "micro-design-layout";
 import {
-  message,
+  // message,
   Button,
   Input,
   Switch,
@@ -32,17 +34,16 @@ import {
   Menu,
 } from "ant-design-vue";
 import {
-  getMenuData,
-  clearMenuItem,
+  // getMenuData,
+  // clearMenuItem,
   WaterMark,
   FooterToolbar,
 } from "micro-design-layout";
-import type { RouteContextProps } from "micro-design-layout";
 
-const i18n = (t: string) => t;
-
-export default defineComponent({
+// const i18n = (t: string) => t;
+@Options({
   name: "BasicLayout",
+  mixins: [],
   components: {
     FooterToolbar,
     WaterMark,
@@ -58,73 +59,110 @@ export default defineComponent({
     [Avatar.name]: Avatar,
     [Menu.Item.name]: Menu.Item,
   },
-  setup() {
-    const loading = ref(false);
-
-    const router = useRouter();
-    const { menuData } = getMenuData(clearMenuItem(router.getRoutes()));
-
-    const baseState = reactive<Omit<RouteContextProps, "menuData">>({
-      selectedKeys: [],
-      openKeys: [],
-      collapsed: false,
-    });
-
-    const state = reactive({
-      menuData,
-      splitMenus: true,
-      title: "MicroLayout",
-      logo: "https://alicdn.antdv.com/v2/assets/logo.1ef800a8.svg",
-      navTheme: "dark",
-      layout: "side",
-      fixSiderbar: true,
-    });
-    const breadcrumb = computed(() =>
-      router.currentRoute.value.matched.concat().map((item) => {
-        return {
-          path: item.path,
-          breadcrumbName: item.meta.title || "",
-        };
-      })
-    );
-
-    const handleCollapsed = () => {
-      baseState.collapsed = !baseState.collapsed;
-    };
-    watchEffect(() => {
-      if (router.currentRoute) {
-        const matched = router.currentRoute.value.matched.concat();
-        baseState.selectedKeys = matched
-          .filter((r) => r.name !== "index")
-          .map((r) => r.path);
-        baseState.openKeys = matched
-          .filter((r) => r.path !== router.currentRoute.value.path)
-          .map((r) => r.path);
-      }
-    });
-
-    function handlePageLoadingClick() {
-      loading.value = true;
-      setTimeout(() => {
-        loading.value = false;
-      }, 2000);
-    }
-
+  data() {
     return {
-      i18n,
-      baseState,
-      state,
-      loading,
-      breadcrumb,
-
-      handlePageLoadingClick,
-      handleCollapsed,
-      handleSearch: () => {
-        message.info("search..");
-      },
+      bigsize: false,
     };
   },
-});
+  methods: {
+    sizeClick() {
+      this.$data.bigsize = !this.$data.bigsize;
+    },
+  },
+  computed: {},
+  mounted() {
+    document.body.classList.add("userLayout");
+  },
+  beforeDestroy() {
+    document.body.classList.remove("userLayout");
+  },
+})
+export default class UserLayout extends Vue {}
+
+// export default defineComponent({
+//   name: "BasicLayout",
+//   components: {
+//     FooterToolbar,
+//     WaterMark,
+
+//     [Button.name]: Button,
+//     [Input.name]: Input,
+//     [Input.Search.name]: Input.Search,
+//     [Switch.name]: Switch,
+//     [Select.name]: Select,
+//     [Select.Option.displayName!]: Select.Option,
+//     [Space.name]: Space,
+//     [Badge.name]: Badge,
+//     [Avatar.name]: Avatar,
+//     [Menu.Item.name]: Menu.Item,
+//   },
+//   setup() {
+//     const loading = ref(false);
+
+//     const router = useRouter();
+//     const { menuData } = getMenuData(clearMenuItem(router.getRoutes()));
+
+//     const baseState = reactive<Omit<RouteContextProps, "menuData">>({
+//       selectedKeys: [],
+//       openKeys: [],
+//       collapsed: false,
+//     });
+
+//     const state = reactive({
+//       menuData,
+//       splitMenus: true,
+//       title: "MicroLayout",
+//       logo: "https://alicdn.antdv.com/v2/assets/logo.1ef800a8.svg",
+//       navTheme: "dark",
+//       layout: "side",
+//       fixSiderbar: true,
+//     });
+//     const breadcrumb = computed(() =>
+//       router.currentRoute.value.matched.concat().map((item) => {
+//         return {
+//           path: item.path,
+//           breadcrumbName: item.meta.title || "",
+//         };
+//       })
+//     );
+
+//     const handleCollapsed = () => {
+//       baseState.collapsed = !baseState.collapsed;
+//     };
+//     watchEffect(() => {
+//       if (router.currentRoute) {
+//         const matched = router.currentRoute.value.matched.concat();
+//         baseState.selectedKeys = matched
+//           .filter((r) => r.name !== "index")
+//           .map((r) => r.path);
+//         baseState.openKeys = matched
+//           .filter((r) => r.path !== router.currentRoute.value.path)
+//           .map((r) => r.path);
+//       }
+//     });
+
+//     function handlePageLoadingClick() {
+//       loading.value = true;
+//       setTimeout(() => {
+//         loading.value = false;
+//       }, 2000);
+//     }
+
+//     return {
+//       i18n,
+//       baseState,
+//       state,
+//       loading,
+//       breadcrumb,
+
+//       handlePageLoadingClick,
+//       handleCollapsed,
+//       handleSearch: () => {
+//         message.info("search..");
+//       },
+//     };
+//   },
+// });
 </script>
 <style lang="less" scoped>
 #userLayout.user-layout-wrapper {
