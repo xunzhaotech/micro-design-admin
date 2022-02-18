9045f822f2883c88482de4cfaf81e5b13c9aba5c
luyb <luyb@xunzhaotech.com>
2022-02-18 17:09:16 +0800

    fix -集成页面信息设置

--- src/layouts/UserLayout/index.vue
+++ src/layouts/UserLayout/index.vue
@@ -5,7 +5,7 @@
  * @LastEditors: xunzhaotech
 -->
 <template>
-  <div id="userLayout"></div>
+  <div id="userLayout" class="user-layout-wrapper">sfsfsf</div>
   <router-view />
   <div class="footer">
     <div class="links">
@@ -13,7 +13,7 @@
       <a href="_self">隐私</a>
       <a href="_self">条款</a>
     </div>
-    <div class="copyright">Copyright &copy; 2018 vueComponent</div>
+    <div class="copyright">Copyright &copy; 2020 vueComponent</div>
   </div>
 </template>
 
@@ -126,3 +126,102 @@ export default defineComponent({
   },
 });
 </script>
+<style lang="less" scoped>
+#userLayout.user-layout-wrapper {
+  height: 100%;
+
+  &.mobile {
+    .container {
+      .main {
+        max-width: 368px;
+        width: 98%;
+      }
+    }
+  }
+
+  .container {
+    width: 100%;
+    min-height: 100%;
+    background: #f0f2f5 url(~@/assets/background.svg) no-repeat 50%;
+    background-size: 100%;
+    padding: 110px 0 144px;
+    position: relative;
+
+    a {
+      text-decoration: none;
+    }
+
+    .top {
+      text-align: center;
+
+      .header {
+        height: 44px;
+        line-height: 44px;
+
+        .badge {
+          position: absolute;
+          display: inline-block;
+          line-height: 1;
+          vertical-align: middle;
+          margin-left: -12px;
+          margin-top: -10px;
+          opacity: 0.8;
+        }
+
+        .logo {
+          height: 44px;
+          vertical-align: top;
+          margin-right: 16px;
+          border-style: none;
+        }
+
+        .title {
+          font-size: 33px;
+          color: rgba(0, 0, 0, 0.85);
+          font-family: Avenir, "Helvetica Neue", Arial, Helvetica, sans-serif;
+          font-weight: 600;
+          position: relative;
+          top: 2px;
+        }
+      }
+      .desc {
+        font-size: 14px;
+        color: rgba(0, 0, 0, 0.45);
+        margin-top: 12px;
+        margin-bottom: 40px;
+      }
+    }
+
+    .main {
+      min-width: 260px;
+      width: 368px;
+      margin: 0 auto;
+    }
+
+    .footer {
+      position: absolute;
+      width: 100%;
+      bottom: 0;
+      padding: 0 16px;
+      margin: 48px 0 24px;
+      text-align: center;
+
+      .links {
+        margin-bottom: 8px;
+        font-size: 14px;
+        a {
+          color: rgba(0, 0, 0, 0.45);
+          transition: all 0.3s;
+          &:not(:last-child) {
+            margin-right: 40px;
+          }
+        }
+      }
+      .copyright {
+        color: rgba(0, 0, 0, 0.45);
+        font-size: 14px;
+      }
+    }
+  }
+}
+</style>
