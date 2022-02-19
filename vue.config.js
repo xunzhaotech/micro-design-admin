c8febe11ad8fb9fe68d8d3897ee672fe7fe40b63
luyb <luyb@xunzhaotech.com>
2022-02-19 09:12:49 +0800

    fix -添加项目基础配置

--- vue.config.js
+++ vue.config.js
@@ -4,26 +4,51 @@
  * @QQ: 1525572900
  * @LastEditors: xunzhaotech
  */
+const webpack = require("webpack");
 const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
+const dayjs = require("dayjs");
+const pkg = require("./package.json");
 // const { config } = require("process");
 // const resolve = (dir: string) => path.join(__dirname, dir);
+// chainWebpack: (config:Config) => {
 console.log(path.resolve());
+const __APP_INFO__ = {
+  pkg,
+  lastBuildTime: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+};
 module.exports = defineConfig({
   transpileDependencies: true,
-  // chainWebpack: (config:Config) => {
-  //   config.resolve.alias
-  //   .set('@', resolve('src'))
-  //   .set('@img',resolve('src/assets/img'))
-  //   .set('@styl',resolve('src/assets/styl'))
-  //   .set('@js',resolve('src/assets/js'))
-  //   .set('@ts',resolve('src/assets/ts'))
-  //   .set('@fonts', resolve('src/assets/fonts'))
-  //   .set('@css', resolve('src/assets/css'))
-  //   .set('@libs',resolve('src/libs'))
-  //   .set('@cp',resolve('src/components'))
-  //   .set('@views',resolve('src/views'))
-  //   .set('@plugins',resolve('src/plugins'))
-  //   .end()
-  // },
+  lintOnSave: true, //关闭eslint检查
+  chainWebpack: (config) => {
+    console.log(11111111);
+    console.log(config.mode);
+    //   config.resolve.alias
+    //   .set('@', resolve('src'))
+    //   .set('@img',resolve('src/assets/img'))
+    //   .set('@styl',resolve('src/assets/styl'))
+    //   .set('@js',resolve('src/assets/js'))
+    //   .set('@ts',resolve('src/assets/ts'))
+    //   .set('@fonts', resolve('src/assets/fonts'))
+    //   .set('@css', resolve('src/assets/css'))
+    //   .set('@libs',resolve('src/libs'))
+    //   .set('@cp',resolve('src/components'))
+    //   .set('@views',resolve('src/views'))
+    //   .set('@plugins',resolve('src/plugins'))
+    //   .end()
+  },
+  configureWebpack: (config) => {
+    // 开启顶级await
+    config.experiments = {
+      topLevelAwait: true,
+    };
+    config.resolve.fallback = { path: require.resolve("path-browserify") };
+
+    config.plugins.push(
+      // 定义全局变量
+      new webpack.DefinePlugin({
+        __APP_INFO__: JSON.stringify(__APP_INFO__),
+      })
+    );
+  },
 });
