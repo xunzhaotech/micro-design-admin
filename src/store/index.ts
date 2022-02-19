08b167347e22f909a958977525ed3915205aade9
luyb <luyb@xunzhaotech.com>
2022-02-19 13:44:21 +0800

    fix -修改基础项目设置

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,13 @@
 import { createStore } from "vuex";
-export default createStore({
-  state: {},
+export interface State {
+  count: number;
+}
+// 定义 injection key
+// export const key: InjectionKey<Store<State>> = Symbol();
+export default createStore<State>({
+  state: {
+    count: 0,
+  },
   getters: {},
   mutations: {},
   actions: {},
