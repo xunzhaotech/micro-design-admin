949b5549e7689731764ffca445bd7705d79f6160
luyb <luyb@xunzhaotech.com>
2022-02-22 15:37:59 +0800

    fix -修改基础信息设置

---
+++ src/core/permission/modules/types.ts
@@ -0,0 +1,8 @@
+import type { SysPermissionType } from "./sys";
+import type { NetdiskPermissionType } from "./netdisk";
+
+export type PermissionType = ReplaceAll<
+  SysPermissionType | NetdiskPermissionType,
+  "/",
+  "."
+>;
