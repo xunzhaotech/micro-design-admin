e0aef5155d0aeb977528efc2be6ac8c378666931
luyb <luyb@xunzhaotech.com>
2022-03-31 11:17:47 +0800

    fix -修改基础信息设置

--- src/views/system/user/login/index.vue
+++ src/views/system/user/login/index.vue
@@ -7,6 +7,9 @@
 <script lang="ts">
 import { mapState } from "vuex";
 import { Options, Vue } from "vue-class-component";
+// import { Watch } from "vue-property-decorator";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Options({
@@ -38,17 +41,40 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
   },
 })
 export default class HomeView extends Vue {
+  store = useStore();
+  router = useRouter();
+  message = 110;
+  testString = this.store.state.testModule.test;
   private name = "123";
   private msg = "234";
   // super(name: string, msg: string) {
   //   this.name = name;
   //   this.age = age;
   // }
-
+  // @Watch("message", { immediate: true, deep: true })
+  add() {
+    this.message++;
+  }
+  less() {
+    this.message--;
+  }
+  mounted() {
+    console.log(this.router.getRoutes());
+    console.log(this.store);
+    console.log(this.store.state);
+    console.log(this.store.state.testModule.test);
+  }
   public clickFunc(): void {
     console.log(this.name);
     console.log(this.msg);
   }
+  // onChangeValue(newVal: number, oldVal: number) {
+  //   // todo...
+  //   // console.log(newVal);
+  //   if (newVal > 110) {
+  //     this.store.commit("testModule/updateTestData", newVal);
+  //   }
+  // }
 }
 </script>
 
