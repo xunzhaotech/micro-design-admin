e7c9db2e727c808cde2e77be7488169dd4f8e953
luyb <luyb@xunzhaotech.com>
2022-12-02 12:39:18 +0800

    fix -修改打包基础信息设置

--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++ docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -1,4 +1,4 @@
-import clientAppRootComponent0 from 'D:/项目资料/project/micro-design-admin/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+import clientAppRootComponent0 from 'D:/DocsAwesome/micro-design-admin/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
 
 export const clientAppRootComponents = [
   clientAppRootComponent0,
--- docs/.vuepress/.temp/internal/layoutComponents.js
+++ docs/.vuepress/.temp/internal/layoutComponents.js
@@ -1,6 +1,6 @@
 import { defineAsyncComponent } from 'vue'
 
 export const layoutComponents = {
-  "404": defineAsyncComponent(() => import("D:/项目资料/project/micro-design-admin/node_modules/@vuepress/theme-default/lib/client/layouts/404.vue")),
-  "Layout": defineAsyncComponent(() => import("D:/项目资料/project/micro-design-admin/node_modules/@vuepress/theme-default/lib/client/layouts/Layout.vue")),
+  "404": defineAsyncComponent(() => import("D:/DocsAwesome/micro-design-admin/node_modules/@vuepress/theme-default/lib/client/layouts/404.vue")),
+  "Layout": defineAsyncComponent(() => import("D:/DocsAwesome/micro-design-admin/node_modules/@vuepress/theme-default/lib/client/layouts/Layout.vue")),
 }
